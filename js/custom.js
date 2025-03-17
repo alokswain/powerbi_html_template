eb85731917bf09c2e4a4c719ad7a48561003c33d
alokswain <alok.swain@magnondesignory.com>
2025-03-17 17:54:17 +0530

    get the original template back

--- js/custom.js
+++ js/custom.js
@@ -1,57 +1,31 @@
 
-  // (function ($) {
+  (function ($) {
   
-  // 'use strict';
+  "use strict";
 
-  //   // PRE LOADER
-  //   $(window).load(function(){
-  //     $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
-  //   });
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+    });
 
-  //   // NAVBAR
-  //   // $('.navbar').headroom();
+    // NAVBAR
+    $(".navbar").headroom();
 
-  //   $('.navbar-collapse a').click(function(){
-  //       $('.navbar-collapse').collapse('hide');
-  //   });
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
 
-  //   $('.slick-slideshow').slick({
-  //     autoplay: true,
-  //     infinite: true,
-  //     arrows: false,
-  //     fade: true,
-  //     dots: true,
-  //   });
+    $('.slick-slideshow').slick({
+      autoplay: true,
+      infinite: true,
+      arrows: false,
+      fade: true,
+      dots: true,
+    });
 
-  //   $('.slick-testimonial').slick({
-  //     arrows: false,
-  //     dots: true,
-  //   });
+    $('.slick-testimonial').slick({
+      arrows: false,
+      dots: true,
+    });
     
-  // })(window.jQuery);
-
-
-  window.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
-    // check for input element change and switch accordingly
-
-    document.getElementById('bi_value').addEventListener('change', goToTab);
-
-    function goToTab() {
-      const inputValue = this.value;
-      var links = document.getElementById('pills-tab');
-      switch (inputValue) {
-        case '1':
-          links.children[0].children[0].click();
-          break;
-        case '2':
-          links.children[1].children[0].click();
-          break;
-        case '3':
-          links.children[2].children[0].click();
-          break;
-        default:
-          console.log(`Sorry, we are out of ${expr}.`);
-      }
-    }
-  });
+  })(window.jQuery);
