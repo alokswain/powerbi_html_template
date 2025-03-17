771380dbfa3ff87d20a246d4ac83b688df8a4c44
alokswain <alok.swain@magnondesignory.com>
2025-03-17 16:49:16 +0530

    src changes

--- js/custom1.js
+++ js/custom1.js
@@ -1,20 +1,29 @@
-window.addEventListener('load', (event) => {
-  document.getElementById('bi_value').addEventListener('change', goToTab);
-  document.getElementById('bi_value').addEventListener('keyup', goToTab);
-  function goToTab() {
-    const inputValue = this.value;
-    switch (inputValue) {
-      case '1':
-        document.getElementById('bi_txt').innerHTML = inputValue;
-        break;
-      case '2':
-        document.getElementById('bi_txt').innerHTML = inputValue;
-        break;
-      case '3':
-        document.getElementById('bi_txt').innerHTML = inputValue;
-        break;
-      default:
-        document.getElementById('bi_txt').innerHTML = inputValue;
-    }
-  }
+document.getElementById('bi_value').addEventListener('change', function(){
+  setTimeout(function(){
+    goToTab()
+  }, 3000);
 });
+document.getElementById('bi_value').addEventListener('keyup', function(){
+  goToTab();
+});
+
+setTimeout(function(){
+    goToTab()
+  }, 3000);
+
+function goToTab() {
+        var inputValue = document.getElementById('bi_value').value;
+        switch (inputValue) {
+        case '1':
+            document.getElementById('bi_txt').innerHTML = inputValue;
+            break;
+        case '2':
+            document.getElementById('bi_txt').innerHTML = inputValue;
+            break;
+        case '3':
+            document.getElementById('bi_txt').innerHTML = inputValue;
+            break;
+        default:
+            document.getElementById('bi_txt').innerHTML = inputValue;
+        }
+}
