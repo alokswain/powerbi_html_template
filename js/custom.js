cfef66e18786bd161934ff3afe6975ac88ca7f53
alokswain <alok.swain@magnondesignory.com>
2025-03-19 20:36:19 +0530

    remove document ready things

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,6 @@
 
-  (function ($) {
-  "use strict";
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
-    });
+    $('.preloader').delay(500).slideUp('slow');   
     $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
-  })(window.jQuery);
+
